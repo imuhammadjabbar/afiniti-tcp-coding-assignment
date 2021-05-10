5583bbf4782ab53034fc0ef67c0eae5c7adbb1a3
imuhammadjabbar@gmail.com <imuhammadjabbar@gmail.com>
2021-05-10 17:58:22 +0500

    Initial Project Setup

---
+++ afiniti-tcp-coding-assignment/ViewControllers Layer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  afiniti-tcp-coding-assignment
+//
+//  Created by Jabbar on 10/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
